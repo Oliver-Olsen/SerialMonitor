2e63fe301770cff86c1cba2f2beb06860750b108
Oliver Olsen <oliver@skou-olsen.dk>
2025-01-08 10:08:27 +0100

    DEC to char

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ void loop() {
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
     Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
+    Serial.println((char)incomingByte);
   }
 }
 
